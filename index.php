50ff0b1f301a3e10180b1bf51fc87550af5ef888
anthony <anthonyabisaid1@gmail.com>
2024-11-22 15:43:44 +0200

    adding home page and 404 page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="/public/favicon.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="logo">Clinic.io</div>
+        <div class="nav-links">
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+            <a href="#">About</a>
+        </div>
+        <button class="register-btn">Register</button>
+    </nav>
+
+    <div class="hero">
+        <div class="hero-content">
+            <h1 class="hero-title">Need a <br> monthly <br>check up?</h1>
+            <p class="hero-subtitle">We're here to help. Talk to someone today.</p>
+        </div>
+        <div class="illustration">
+            <img src="/public/DrawKit Vector Illustration Health & Medical (3).svg"></img>
+        </div>
+    </div>
+    <footer>
+        &copy; 2024 health.io. All rights reserved.
+    </footer>
+</body>
+
+</html>
